3f2cf17c16b9f062bac037bd372085417e9f854f
yazu <appomsk@gmail.com>
2017-09-19 16:42:52 +0300

    Added keys module for getting keys from .trc file

---
+++ tvitor/keys.py
@@ -0,0 +1,18 @@
+import re
+import os
+
+RCFILE=os.path.join(os.environ['HOME'], '.trc')
+
+
+def keys(rcfile=RCFILE, user=None):
+    rc = open(rcfile).read()
+
+    if not user:
+        user = re.findall(r'default_profile:\n\s+-\s+(\w+?)\n', rc)[0]
+
+    keys = {}
+    for s in re.findall(r'username:.*?\n(?:.*?\n){4}', rc):
+        v = re.split(r'[:\s]+', s)
+        keys[v[1]] = [v[3], v[5], v[7], v[9]]
+
+    return keys[user]
